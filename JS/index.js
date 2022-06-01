ec17a0f0f9d679c9d5d1b36540e9ba95aee3255a
Marianela Carrasco <marianela.carrascoi36@gmail.com>
2022-06-01 18:21:05 -0300

    Primer trabajo de javascript

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+// el usuario ingresara N cantidad de entradas
+
+let entrada= +(prompt("Ingrese cantidad de entradas"));
+while(entrada!= 0){
+switch(entrada){
+     case 1:
+         alert("Ya tienes tu entrada")
+         break;
+         
+     case 2:
+         alert("Ya tienes tu entrada");
+         break;
+         
+     case 3:
+         alert("Ya tienes tu entrada");
+         break;
+         
+     default:
+         alert("A excedido el limite de entradas por personas");    
+}
+entrada=+(prompt("Ingrese cantidad de entradas"));
+}
